c502ec383b9b364e9a52778167398754a52d30ae
basicali <103008963+basicali@users.noreply.github.com>
2022-04-05 10:50:20 -0700

    Initialize dependabot/npm_and_yarn/example/ansi-regex-4.1.1

---
+++ android/src/main/java/com/reactnativestripesdk/AuBECSDebitFormViewManager.kt
@@ -0,0 +1,32 @@
+package com.reactnativestripesdk
+
+import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.common.MapBuilder
+import com.facebook.react.uimanager.SimpleViewManager
+import com.facebook.react.uimanager.ThemedReactContext
+import com.facebook.react.uimanager.annotations.ReactProp
+
+class AuBECSDebitFormViewManager : SimpleViewManager<AuBECSDebitFormView>() {
+  override fun getName() = "AuBECSDebitForm"
+
+  override fun getExportedCustomDirectEventTypeConstants(): MutableMap<String, Any> {
+    return MapBuilder.of(
+      FormCompleteEvent.EVENT_NAME, MapBuilder.of("registrationName", "onCompleteAction"))
+  }
+
+  @ReactProp(name = "companyName")
+  @SuppressWarnings("unused")
+  fun setCompanyName(view: AuBECSDebitFormView, name: String?) {
+    view.setCompanyName(name)
+  }
+
+  @ReactProp(name = "formStyle")
+  @SuppressWarnings("unused")
+  fun setFormStyle(view: AuBECSDebitFormView, style: ReadableMap) {
+    view.setFormStyle(style)
+  }
+
+  override fun createViewInstance(reactContext: ThemedReactContext): AuBECSDebitFormView {
+    return AuBECSDebitFormView(reactContext)
+  }
+}
