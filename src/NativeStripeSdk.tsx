2e3ddedbf0d6a54927ffdb2b0af15989b2d8be67
basicali <103008963+basicali@users.noreply.github.com>
2022-04-05 10:50:19 -0700

    Initialize dependabot/npm_and_yarn/example/plist-3.0.5

---
+++ src/NativeStripeSdk.tsx
@@ -0,0 +1,84 @@
+import { NativeModules } from 'react-native';
+import type {
+  PaymentMethodCreateParams,
+  ApplePay,
+  PaymentSheet,
+  ConfirmSetupIntent,
+  InitialiseParams,
+  CreatePaymentMethodResult,
+  RetrievePaymentIntentResult,
+  RetrieveSetupIntentResult,
+  ConfirmPaymentResult,
+  HandleNextActionResult,
+  ConfirmSetupIntentResult,
+  CreateTokenForCVCUpdateResult,
+  InitPaymentSheetResult,
+  PresentPaymentSheetResult,
+  ConfirmPaymentSheetPaymentResult,
+  ApplePayResult,
+  CreateTokenResult,
+  GooglePayInitResult,
+  PayWithGooglePayResult,
+  CreateGooglePayPaymentMethodResult,
+  GooglePay,
+  OpenApplePaySetupResult,
+  CreateTokenParams,
+} from './types';
+
+type NativeStripeSdkType = {
+  initialise(params: InitialiseParams): Promise<void>;
+  createPaymentMethod(
+    data: PaymentMethodCreateParams.Params,
+    options: PaymentMethodCreateParams.Options
+  ): Promise<CreatePaymentMethodResult>;
+  handleNextAction(
+    paymentIntentClientSecret: string
+  ): Promise<HandleNextActionResult>;
+  confirmPayment(
+    paymentIntentClientSecret: string,
+    data: PaymentMethodCreateParams.Params,
+    options: PaymentMethodCreateParams.Options
+  ): Promise<ConfirmPaymentResult>;
+  isApplePaySupported(): Promise<boolean>;
+  presentApplePay(params: ApplePay.PresentParams): Promise<ApplePayResult>;
+  confirmApplePayPayment(clientSecret: string): Promise<void>;
+  updateApplePaySummaryItems(
+    summaryItems: ApplePay.CartSummaryItem[],
+    errorAddressFields: Array<{
+      field: ApplePay.AddressFields;
+      message?: string;
+    }>
+  ): Promise<void>;
+  confirmSetupIntent(
+    paymentIntentClientSecret: string,
+    data: ConfirmSetupIntent.Params,
+    options: ConfirmSetupIntent.Options
+  ): Promise<ConfirmSetupIntentResult>;
+  retrievePaymentIntent(
+    clientSecret: string
+  ): Promise<RetrievePaymentIntentResult>;
+  retrieveSetupIntent(clientSecret: string): Promise<RetrieveSetupIntentResult>;
+  initPaymentSheet(
+    params: PaymentSheet.SetupParams
+  ): Promise<InitPaymentSheetResult>;
+  presentPaymentSheet(): Promise<PresentPaymentSheetResult>;
+  confirmPaymentSheetPayment(): Promise<ConfirmPaymentSheetPaymentResult>;
+  createTokenForCVCUpdate(cvc: string): Promise<CreateTokenForCVCUpdateResult>;
+  handleURLCallback(url: string): Promise<boolean>;
+  createToken(params: CreateTokenParams): Promise<CreateTokenResult>;
+  isGooglePaySupported(
+    params: GooglePay.IsGooglePaySupportedParams
+  ): Promise<boolean>;
+  initGooglePay(params: GooglePay.InitParams): Promise<GooglePayInitResult>;
+  presentGooglePay(
+    params: GooglePay.PresentGooglePayParams
+  ): Promise<PayWithGooglePayResult>;
+  createGooglePayPaymentMethod(
+    params: GooglePay.CreatePaymentMethodParams
+  ): Promise<CreateGooglePayPaymentMethodResult>;
+  openApplePaySetup(): Promise<OpenApplePaySetupResult>;
+};
+
+const { StripeSdk } = NativeModules;
+
+export default StripeSdk as NativeStripeSdkType;
---
+++ src/types/index.ts
@@ -0,0 +1,244 @@
+import type { Card } from './Card';
+import type {
+  ApplePayError,
+  CardActionError,
+  ConfirmPaymentError,
+  ConfirmSetupIntentError,
+  CreatePaymentMethodError,
+  CreateTokenError,
+  GooglePayError,
+  PaymentSheetError,
+  RetrievePaymentIntentError,
+  RetrieveSetupIntentError,
+  StripeError,
+} from './Errors';
+import type { PaymentIntent } from './PaymentIntents';
+import type { PaymentMethod } from './PaymentMethods';
+import type { PaymentSheet } from './PaymentSheet';
+import type { SetupIntent } from './SetupIntent';
+import type { ThreeDSecureConfigurationParams } from './ThreeDSecure';
+
+export * from './ApplePay';
+export * from './PaymentIntents';
+export * from './PaymentMethods';
+export * from './SetupIntent';
+export * from './GooglePay';
+export * from './ThreeDSecure';
+export * from './components/ApplePayButtonComponent';
+export * from './components/AuBECSDebitForm';
+export * from './components/CardFieldInput';
+export * from './components/CardFormView';
+export * from './Card';
+export * from './Errors';
+export * from './PaymentSheet';
+
+/**
+ * @ignore
+ */
+export type Dictionary<T> = {
+  [key: string]: T;
+};
+
+/**
+ * @ignore
+ */
+export type Nullable<T> = T | null;
+
+export interface AppInfo {
+  name?: string;
+  partnerId?: string;
+  url?: string;
+  version?: string;
+}
+
+export type CreatePaymentMethodResult =
+  | {
+      paymentMethod: PaymentMethod;
+      error?: undefined;
+    }
+  | {
+      paymentMethod?: undefined;
+      error: StripeError<CreatePaymentMethodError>;
+    };
+
+export type RetrievePaymentIntentResult =
+  | {
+      paymentIntent: PaymentIntent;
+      error?: undefined;
+    }
+  | {
+      paymentIntent?: undefined;
+      error: StripeError<RetrievePaymentIntentError>;
+    };
+
+export type RetrieveSetupIntentResult =
+  | {
+      setupIntent: SetupIntent;
+      error?: undefined;
+    }
+  | {
+      setupIntent?: undefined;
+      error: StripeError<RetrieveSetupIntentError>;
+    };
+
+export type ConfirmPaymentResult =
+  | {
+      paymentIntent: PaymentIntent;
+      error?: undefined;
+    }
+  | {
+      paymentIntent?: undefined;
+      error: StripeError<ConfirmPaymentError>;
+    };
+
+export type HandleNextActionResult =
+  | {
+      paymentIntent: PaymentIntent;
+      error?: undefined;
+    }
+  | {
+      paymentIntent?: undefined;
+      error: StripeError<CardActionError>;
+    };
+
+export type ConfirmSetupIntentResult =
+  | {
+      setupIntent: SetupIntent;
+      error?: undefined;
+    }
+  | {
+      setupIntent?: undefined;
+      error: StripeError<ConfirmSetupIntentError>;
+    };
+
+export type CreateTokenForCVCUpdateResult =
+  | {
+      tokenId: string;
+      error?: undefined;
+    }
+  | {
+      tokenId?: undefined;
+      error: StripeError<ConfirmSetupIntentError>;
+    };
+
+export type InitPaymentSheetResult =
+  | {
+      paymentOption?: PaymentSheet.PaymentOption;
+      error?: undefined;
+    }
+  | {
+      paymentOption?: undefined;
+      error: StripeError<PaymentSheetError>;
+    };
+
+export type PresentPaymentSheetResult =
+  | {
+      paymentOption?: undefined;
+      error?: undefined;
+    }
+  | {
+      paymentOption?: PaymentSheet.PaymentOption;
+      error?: undefined;
+    }
+  | {
+      paymentOption?: undefined;
+      error: StripeError<PaymentSheetError>;
+    };
+
+export type CreateTokenResult =
+  | {
+      token: Card.Token;
+      error?: undefined;
+    }
+  | {
+      token?: undefined;
+      error: StripeError<CreateTokenError>;
+    };
+
+export type ConfirmPaymentSheetPaymentResult = {
+  error?: StripeError<PaymentSheetError>;
+};
+
+export type ApplePayResult =
+  | {
+      paymentMethod: PaymentMethod;
+      error?: undefined;
+    }
+  | {
+      paymentMethod?: undefined;
+      error: StripeError<ApplePayError>;
+    };
+
+export interface InitStripeParams {
+  publishableKey: string;
+  merchantIdentifier?: string;
+  threeDSecureParams?: ThreeDSecureConfigurationParams;
+  stripeAccountId?: string;
+  urlScheme?: string;
+  setReturnUrlSchemeOnAndroid?: boolean;
+  /**
+   * @deprecated Use setReturnUrlSchemeOnAndroid instead
+   */
+  setUrlSchemeOnAndroid?: boolean;
+}
+
+export interface InitialiseParams extends InitStripeParams {
+  appInfo: AppInfo;
+}
+
+export type GooglePayInitResult =
+  | {
+      error?: undefined;
+    }
+  | {
+      error: StripeError<GooglePayError>;
+    };
+
+export type PayWithGooglePayResult =
+  | {
+      error?: undefined;
+    }
+  | {
+      error: StripeError<GooglePayError>;
+    };
+
+export type CreateGooglePayPaymentMethodResult =
+  | {
+      paymentMethod: PaymentMethod;
+      error?: undefined;
+    }
+  | {
+      paymentMethod?: undefined;
+      error: StripeError<GooglePayError>;
+    };
+
+export type OpenApplePaySetupResult =
+  | {
+      error?: undefined;
+    }
+  | {
+      error: StripeError<ApplePayError>;
+    };
+
+export type CreateTokenParams =
+  | CreateTokenCardParams
+  | CreateTokenBankAccountParams;
+
+export type CreateTokenCardParams = {
+  type: 'Card';
+  address?: Card.Address;
+  name?: string;
+  currency?: string;
+};
+
+export type BankAcccountHolderType = 'Company' | 'Individual';
+
+export type CreateTokenBankAccountParams = {
+  type: 'BankAccount';
+  accountHolderName?: string;
+  accountHolderType?: BankAcccountHolderType;
+  accountNumber: string;
+  country: string;
+  currency: string;
+  routingNumber?: string;
+};
